b3bdb4cd4231aed5aca65bb09ccf1813e009ae52
Alex Cuadra <acuadraq@gmail.com>
2022-09-28 14:36:55 -0600

    adding cat logo

---
+++ front/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CatLogo from "../../assets/CatwikiLogo.svg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={CatLogo} alt="Cat Wiki Logo" />
+    </div>
+  );
+};
+
+export default Navbar;
