b8eb751e494c708999af8386e434cef3b70890a9
Alex Cuadra <acuadraq@gmail.com>
2022-09-28 22:45:04 -0600

    feature: adding services

---
+++ front/src/services/http.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosResponse } from "axios";
+import { CatBreed, MostSearchCats, SingleCat } from "models/Cats";
+
+const baseUrl = process.env.API_URL;
+
+const fetchApi = async <T>(extension: string, query?: string): Promise<T> => {
+  const request: AxiosResponse = await axios(
+    `${baseUrl}${extension}${query || ""}`,
+  );
+  return request.data;
+};
+
+export const getCatByBreed = (query: string) =>
+  fetchApi<CatBreed[]>("/cat/breeds", `?search=${query}`);
+
+export const getCatById = (id: string) =>
+  fetchApi<SingleCat[]>(`/cat/breeds/${id}`, "?limit=8");
+
+export const getMostSearch = () =>
+  fetchApi<MostSearchCats[]>("/cat/most-search");
