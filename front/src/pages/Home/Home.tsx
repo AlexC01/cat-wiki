e0e92ff28af9edfc6303e00930654c5c74696db4
Alex Cuadra <acuadraq@gmail.com>
2022-09-28 22:27:42 -0600

    adding hero banner

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import CatLogo from "../../assets/CatwikiLogo.svg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="hero-styles rounded-tl-boxes rounded-tr-boxes bg-no-repeat">
+      <div className="text-white pl-12 lg:pl-20 pt-16 lg:pt-24">
+        <div>
+          <img src={CatLogo} alt="Cat Logo" className="invert-color cat-logo" />
+          <p className="text-lg xl:text-2xl mt-3">
+            Get to know more about your
+          </p>
+          <p className="text-lg xl:text-2xl">cat breed</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
