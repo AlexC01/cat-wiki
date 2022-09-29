17aa36752009174c4084fe21262e7d84a8e8d222
Alex Cuadra <acuadraq@gmail.com>
2022-09-28 22:27:54 -0600

    styles: adding css and tailwind config

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
       },
+      backgroundImage: {
+        "hero-lg": "url('/src/assets/HeroImagelg.png')",
+        "hero-md": "url('/src/assets/HeroImagemd.png')",
+        "hero-sm": "url('/src/assets/HeroImagesm.png')",
+      },
+      borderRadius: {
+        boxes: "42px",
+      },
+      spacing: {
+        1440: "1440px",
+      },
     },
   },
   plugins: [],
