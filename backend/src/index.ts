7c073a22722c7d2bd0da3cac6f53c55b2583a039
Alex Cuadra <acuadraq@gmail.com>
2022-10-04 09:42:47 -0600

    feature: adding cors to node

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import catRouter from "./routes/cat";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(catRouter);
 
 app.listen(PORT, () => console.log(`App runing on ${PORT}`));
