a13d793aa97322d2ca5b173e05b2385c42199eba
Alex Cuadra <acuadraq@gmail.com>
2022-10-06 16:02:35 -0600

    feature: adding navigation to home

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import CatLogo from "../../assets/CatwikiLogo.svg";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
     <footer className="footer inset-x-0 bottom-0 w-full px-5 sm:px-10 md:px-16 lg:px-24">
       <div className="justify-center flex flex-row items-center flex-wrap md:justify-between px-16 py-8 bg-black text-white footer">
         <div className="pl-10">
-          <img src={CatLogo} alt="Cat Logo" className="invert-color" />
+          <img
+            onClick={() => navigate("/")}
+            src={CatLogo}
+            alt="Cat Logo"
+            className="invert-color cursor-pointer"
+          />
         </div>
         <div>
           <p>
--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import CatLogo from "../../assets/CatwikiLogo.svg";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <img src={CatLogo} alt="Cat Wiki Logo" />
+      <img
+        onClick={() => navigate("/")}
+        src={CatLogo}
+        alt="Cat Wiki Logo"
+        className="cursor-pointer"
+      />
     </div>
   );
 };
